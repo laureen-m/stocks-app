8ce4681d0e6eb800f2b96a95e1a5f673e3d7a221
laureen-m <marguinlaureen@gmail.com>
2021-03-09 15:22:06 -0600

    create stocks controller + values db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'stocks/index'
+  post 'stocks/create'
+  get '/show/:id', to: 'stocks#show'
   root 'home#index'
+  get '/*path' => 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_194025) do
+ActiveRecord::Schema.define(version: 2021_03_09_212019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "stocks", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "values", force: :cascade do |t|
     t.integer "value"
     t.integer "volume"
     t.date "date"
