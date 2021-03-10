410e66447dbd2800eaec4cec8fc5021cd26f3f4a
laureen-m <marguinlaureen@gmail.com>
2021-03-10 11:58:37 -0600

    clean up code

--- app/javascript/components/Stocks/Stocks.jsx
+++ app/javascript/components/Stocks/Stocks.jsx
@@ -8,37 +8,19 @@ function Stocks() {
     fetch('stocks/index')
       .then(response => response.json())
       .then(data => setStocks(data))
-      .then(console.log(stocks))
       /*.catch(() => history.push('/'))*/;
   }, []);
-
-  /*const allStocks = stocks.map((stock, index) => (
-    <div key={index}>
-      <h5>{stock.name}</h5>
-    </div>
-  ));*/
 
   return (
     <div>
       <h1>List of stocks</h1>
-      {
-        stocks.map(stocks => (
+      { stocks.map(stocks => (
           <div key={stocks.id}>
             <h5>{stocks.name}</h5>
           </div>
-        ))
-      }
-     {/* <div>{allStocks}</div>*/}
+      )) }
       <Link to="/">Home</Link>
     </div>
   );
 }
 export default Stocks;
-
-
-
-/*useEffect(() => {
-    fetch('http://localhost:3000/')
-      .then(response => response.json())
-      .then(console.log)
-  })*/
