ba0502f9b685db5d207b3173271c32e382254807
laureen-m <marguinlaureen@gmail.com>
2021-03-29 15:24:26 -0500

    play with stock data

--- app/models/stock_data.rb
+++ app/models/stock_data.rb
@@ -1,16 +1,17 @@
 class StockData < Kimurai::Base
   @name = "stock_data"
   @engine = :mechanize
-  @start_urls = ["https://www.bloomberg.com/markets2/api/intraday/FLGT%3AUS?days=1&interval=1&volumeInterval=1"]
-  # Other URLs to add: https://www.bloomberg.com/markets2/api/intraday/SCL%3ACN?days=1&interval=1&volumeInterval=1; https://www.bloomberg.com/markets2/api/intraday/ACB%3AUS?days=1&interval=1&volumeInterval=1; https://www.bloomberg.com/markets2/api/intraday/LSPD%3AUS?days=1&interval=1&volumeInterval=1
+  @start_urls = ["http://example.com"]
+  # Other URLs to add: https://www.bloomberg.com/markets2/api/intraday/FLGT%3AUS?days=1&interval=1&volumeInterval=1; https://www.bloomberg.com/markets2/api/intraday/SCL%3ACN?days=1&interval=1&volumeInterval=1; https://www.bloomberg.com/markets2/api/intraday/ACB%3AUS?days=1&interval=1&volumeInterval=1; https://www.bloomberg.com/markets2/api/intraday/LSPD%3AUS?days=1&interval=1&volumeInterval=1
 
   def parse(response, url:, data: {})
     item = {}
   
-    item[:date] = response.xpath('//pre').text.squish
-    item[:price] = response.xpath('//pre').text.squish
-    item[:volume] = response.xpath('//pre').text.squish
+    item[:date] = response.xpath('//h1').text.squish
+    item[:price] = response.xpath('//p').text.squish
+    item[:volume] = response.xpath('//h1').text.squish
 
-    save_to "data.json", item, format: :json
+    # save_to "data.json", item, format: :json
   end
 end
+
