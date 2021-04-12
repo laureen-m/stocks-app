d9def809f3e58775dd6397346f3a06103b648e41
laureen-m <marguinlaureen@gmail.com>
2021-04-12 09:41:44 -0500

    change style

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Background from "../../assets/images/background.jpg";
+import StockPage from './common/StockPage';
+import Header from './common/Header'
+
+const HomePageStyle = styled.div`
+  background-image: url(${Background});
+  background-repeat: no-repeat;
+  background-size: cover;
+  a {
+    color: black;
+    font-size: 40px;
+    text-decoration: none;
+    position: relative;
+  }
+  a:hover {
+    color: black;
+  }
+  a::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 15px;
+  bottom: 0;
+  left: 0;
+  background-color: yellow;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.3s ease-in-out 0s;
+  }
+  a:hover::before {
+    visibility: visible;
+    transform: scaleX(1);
+  }
+`
 
 export default () => (
-  <div>
-    <Link to="/stocks">List of stocks</Link>
-    <h1>Stocks App</h1>
-      <p>
-        A curated list of stocks to get the best returns.
-      </p> 
-    </div>
-);
+  <StockPage>
+    <HomePageStyle>
+      <Link to="/stocks">Enter</Link>
+    </HomePageStyle>
+  </StockPage>
+)
