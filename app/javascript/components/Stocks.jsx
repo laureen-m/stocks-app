f8fb595bba7f72730b1fab4ede484f02b28aa0b3
laureen-m <marguinlaureen@gmail.com>
2021-04-15 18:41:18 -0500

    add header, styling and remove bootstrap components

--- app/javascript/components/Stocks.jsx
+++ app/javascript/components/Stocks.jsx
@@ -1,26 +1,80 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import styled from "styled-components";
+/*import PageWrapper from './common/PageWrapper';*/
+import Header from './common/Header';
 
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Button from 'react-bootstrap/Button';
+const StocksPageStyle = styled.div`
+select {
+  height: 40px;
+  border-radius: 5px;
+  background-image:
+    linear-gradient(45deg, transparent 50%, black 50%),
+    linear-gradient(135deg, black 50%, transparent 50%),
+    linear-gradient(to right, #ccc, #ccc);
+  background-position:
+    calc(100% - 20px) calc(1em + 2px),
+    calc(100% - 15px) calc(1em + 2px),
+    calc(100% - 2.5em) 0.5em;
+  background-size:
+    5px 5px,
+    5px 5px,
+    1px 1.5em;
+  background-repeat: no-repeat;
+  background-color: white;
+  padding: 5px 60px 5px 15px;
+  margin: 0;    
+  /*-webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;*/
+  box-sizing: border-box;
+  -webkit-appearance: none;
+  -moz-appearance: none;  
+  border-style: none;
+}
+select:focus {
+  background-image:
+    linear-gradient(45deg, black 50%, transparent 50%),
+    linear-gradient(135deg, transparent 50%, black 50%),
+    linear-gradient(to right, #ccc, #ccc);
+  background-position:
+    calc(100% - 15px) 1em,
+    calc(100% - 20px) 1em,
+    calc(100% - 2.5em) 0.5em;
+  background-size:
+    5px 5px,
+    5px 5px,
+    1px 1.5em;
+  background-repeat: no-repeat;
+  outline: 0;
+}
+select:-moz-focusring {
+  color: transparent;
+  text-shadow: 0 0 0 black;
+}
+button {
+  background-color: white;
+  height: 40px;
+  border-radius: 5px;
+  border-style: none;
+  padding: 5px 15px 5px 15px;
+  margin: 15px;
+}
+`
 
 function Stocks() {
   const [stocks, setStocks] = useState([]);
   const [stockId, setStockId] = useState('');
-  const [selectedStock, setSelectedStock] = useState(`Select your stock ▾`)
   let history = useHistory();
 
   useEffect(() => {
     fetch('stocks/index')
       .then(response => response.json())
       .then(data => setStocks(data))
+      .catch(console.log)
   }, []);
 
-  const handleSelect = (event) => {
-    setStockId(event);
-    const sorted = stocks.sort((a, b) => a.id - b.id);
-    setSelectedStock(sorted[--event].name);
+  const handleChange = (event) => {
+    setStockId(event.target.value);
   };
 
   const handleClick = () => {
@@ -28,18 +82,21 @@ function Stocks() {
   };
 
   return (
-    <div>
-      <Link to="/">Home</Link>
+    <StocksPageStyle>
+      <Header />
       <h1>List of stocks</h1>
-      <div className="flex flex-row justify-center">
-        <DropdownButton id="dropdown-basic-button" title={selectedStock} className="ma3" onSelect={handleSelect}>
+      <div className="flex flex-row justify-center items-center">
+        <select onChange={handleChange}>
+          <option>Select your stock</option>
           { stocks.map(stocks => (
-            <Dropdown.Item key={stocks.id} eventKey={stocks.id} className="flex flex-column pa1">{stocks.name}</Dropdown.Item>
-          ))}
-        </DropdownButton>
-        <Button className="ma3" onClick={handleClick}>Analyze</Button>
+              /*<select key={stocks.id}>*/
+                <option key={stocks.id} value={stocks.id}>{stocks.name}</option>
+              /*</select>*/  
+          )) }
+        </select>
+        <button onClick={handleClick}>Analyze</button>
       </div>
-    </div>
+    </StocksPageStyle>
   );
 }
 export default Stocks;
