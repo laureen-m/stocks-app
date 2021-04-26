f16dfdfc3fd51c34ee3c58a1de8343d22773a19d
laureen-m <marguinlaureen@gmail.com>
2021-04-26 15:27:39 -0500

    fix indentation

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -19,9 +19,9 @@ class ValuesController < ApplicationController
 
   private
 
-  def value_params
-    params.require(:value).permit(:stock_id, :date, :time, :price, :volume)
-  end
+    def value_params
+      params.require(:value).permit(:stock_id, :date, :time, :price, :volume)
+    end
 
   #  def scrape
     # binding.pry
