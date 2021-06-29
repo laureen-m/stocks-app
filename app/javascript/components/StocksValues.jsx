9b191a47ac06a75ea9e31d238c1528dacb31afb0
Kayli Brownstein <kaylivbrownstein@gmail.com>
2021-06-29 15:25:36 -0400

    Add comments to StocksValues

--- app/javascript/components/StocksValues.jsx
+++ app/javascript/components/StocksValues.jsx
@@ -5,6 +5,9 @@ import { RiCloseFill } from 'react-icons/ri';
 import { BiErrorCircle } from 'react-icons/bi'; 
 import PageWrapper from './common/PageWrapper';
 
+// KB: Just a note fwiw - at work, we try to use the least amount of custom CSS possible. That doesn't mean you need to change anything! 
+// Just wanted to point out that in a work environment, that would be the preference. There is a decent amount of custom CSS below - I wonder if it's worth splitting it into another file or something.
+
 const StocksValuesStyle = styled.div`
 form {
   display: flex;
@@ -125,6 +128,9 @@ function StocksValues() {
       .then(data => setStocks(data))
       .catch(console.log)
   }, []);
+  
+//   KB: One thing that could help with repetitive form logic is using a forms library. My company uses Formik (https://formik.org/), and I see in a Google search that it's recommended.
+//   I'd suggest looking into it and potentially trying out a refactor with the library (or a similar one). It handles validation, initial values, 
 
   const handleChangeStockId = (event) => {
     setStockId(event.target.value);
@@ -175,6 +181,7 @@ function StocksValues() {
   };*/
 
   const handleClickSave = (event) => {
+//     KB: I don't think I'll review this function or its use too thoroughly, as my understanding is that this was just a temporary thing and that integrating an API would replace this parsing of user inputted data.
     event.preventDefault();
     if (stock_id === "" || price.length == 0 || volume.length == 0) {
       setMessage('Empty fields.');
@@ -242,7 +249,8 @@ function StocksValues() {
             <div className="section">
               <label>Stock name</label>
               <select onChange = {handleChangeStockId}>
-                <option>Select your stock</option>
+                <option>Select your stock</option> 
+// KB: I think this should be a label, rather than the first option in the dropdown. Here's a good example if that helps: https://reactjs.org/docs/forms.html#the-select-tag 
                 { stocks.map(stocks => (
                     /*<select key={stocks.id}>*/
                       <option key={stocks.id} value={stocks.id} name="stock_id" required>{stocks.name}</option>
@@ -256,7 +264,8 @@ function StocksValues() {
                 name="price"
                 rows="15"
                 cols="50"
-                onChange={handleChangePrice}
+                onChange={handleChangePrice} // KB: If you're not going to use a form library, it may be easier/less functions/more visible to define the onChange function in-line, 
+// which would look like onChange={e => setPrice(e.target.value)};
                 value={price}
               />
             </div>
@@ -271,7 +280,8 @@ function StocksValues() {
               />
             </div>
             <div className="validation">
-              <button onClick={handleClickSave} type="submit">Add data!</button>
+              <button onClick={handleClickSave} type="submit">Add data!</button> 
+//            KB: I think maybe the submit button is an element that should be outside of the validation div.
               { hasSucceeded === "yes" 
                 ? <div className="success">
                     <div className="pr1 close"><IoMdCheckmarkCircleOutline /></div>
@@ -279,12 +289,18 @@ function StocksValues() {
                     <div className="pl4 pr0 pointer" onClick={handleClickClose}><RiCloseFill /></div>
                   </div>
                 : hasSucceeded === "no" ?
+//              KB: If you don't use a form manager, I think it's more conventional to set an `errorMessage` (or similar) state, and then check for the presence of error messages, like they do here: https://www.educative.io/edpresso/how-to-show-an-error-message-in-react
                   <div className="error">
                     <div className="pr1 close"><BiErrorCircle /></div>
                     <div className="close">{message}</div>
                     <div className="pl4 pr0 pointer" onClick={handleClickClose}><RiCloseFill /></div>
                   </div>
-                : <p></p>
+                : <p></p> 
+//              KB: If you don't need an empty para, you can just return null instead. But I don't think you need this 3-layer conditional rendering.
+//                 I think you can do something like: {hasSucceeded && <whatever you wanna render on success goes here>}
+//                 And then on the next line: {errorMessage && <whatever you wanna render on error goes here>}
+//                 You might wanna make errorMessage or whatever you would name it an object. Or not, depends what you wanna show.
+//                 Here's an article that uses ^ approach for reference: https://www.educative.io/edpresso/how-to-show-an-error-message-in-react
               }
             </div>
           </form>  
