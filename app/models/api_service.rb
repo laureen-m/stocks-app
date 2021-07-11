381c56caadb46b794fb2cf7b8c6c89f8776ec88f
laureen-m <marguinlaureen@gmail.com>
2021-07-11 11:36:24 -0500

    store api key

--- app/models/api_service.rb
+++ app/models/api_service.rb
@@ -3,10 +3,12 @@ class ApiService
   
   def self.fetch_prices(ticker)
     stocks_found = client.search(keywords: ticker)
+    pry
+    1+1
   end
 
   def self.client
-    @client ||= Alphavantage::Client.new(key: "Y79VPJNXJXFRQEI3")
+    @client ||= Alphavantage::Client.new(key: ENV['ALPHA_VANTAGE_API_KEY'])
   end
 
 end
