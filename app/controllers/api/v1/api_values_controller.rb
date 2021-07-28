d46addee0b17f75d702776e2dae50a4269e5a0b0
laureen-m <marguinlaureen@gmail.com>
2021-07-28 15:46:54 -0500

    api_value controller

--- app/controllers/api/v1/api_values_controller.rb
+++ app/controllers/api/v1/api_values_controller.rb
@@ -4,11 +4,6 @@ class Api::V1::ApiValuesController < ApplicationController
 
   def create
     AlphaVantageApi.save_values(ticker)
-    if api_value
-      render json: api_value
-    else
-      render json: api_value.errors
-    end
   end
 
   private
